d0d161d2bc8567b960c4a857a34c4d409d8a433d
Marisa Wadsworth <marisawadsworth@marisas-air.hub>
2018-07-04 12:06:18 +1200

    Table View has book titles

--- Joanne Wadsworth/ViewController.swift
+++ Joanne Wadsworth/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var data = ["The Matheson Brothers", "Regency Brides", "Highlander Heat", "Billionaire Bodyguards", "Princesses of Myth", "Box Sets"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell();
+        cell.textLabel?.text = data[indexPath.row]
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
